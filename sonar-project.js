cb59fa15d52aa813814e0645de87cba676efc737
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2021-09-23 13:37:13 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.59.54:9000/',
+  serverUrl: 'http://172.31.30.243:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
