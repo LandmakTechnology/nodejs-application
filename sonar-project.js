4820a556c2b236813c85c1f828323f9e9e18a1ba
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2021-12-16 18:09:48 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.30.243:9000/',
+  serverUrl: 'http://3.236.129.45:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '49d6f5bc1b6aad8034f68d9bca8441c73d97a55b',
+	    'sonar.login': '854e4108e6067a3825aa1e8fd18699fd74a1cec4',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
